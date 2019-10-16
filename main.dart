f5c3def1c164b5e05056fd65d1d72050d7365f56
Douglas Barbosa <douglas_b@live.com>
2019-10-16 01:32:14 -0300

    Atualização da timeline
    Barra de notícias atualizada, tópicos organizados e loja com TextField. Todos os campos animados.

--- main.dart
+++ main.dart
@@ -8,10 +8,48 @@ void main() {
 
 class Home extends StatefulWidget {
   @override
+
   _HomeState createState() => _HomeState();
+
 }
 
 class _HomeState extends State<Home> {
+  Container myNews( String imageVal, String heading, String subheading){
+    return Container(
+      width: 160.0,
+      child: Card(
+        child: Wrap(
+          children: <Widget>[
+            Image.network(imageVal),
+            ListTile(
+              title: Text(heading
+              ),
+              subtitle: Text(
+                  subheading),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  GestureDetector myTopic (Color color, String titulo) {
+    return new GestureDetector(
+      onTap: () {
+        print("LAIS OBRIGADO POR TUDO TE AMOOOO");
+      },
+      child: Container(
+        color: color,
+        padding: EdgeInsets.all(20.0),
+        child: Text(
+          titulo,
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+    /* Color(0xff2A236E), */
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,7 +59,7 @@ class _HomeState extends State<Home> {
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.refresh),
-            onPressed: () {},
+            onPressed: () {print("elas que lutem");},
           ),
         ],
       ),
@@ -51,118 +89,90 @@ class _HomeState extends State<Home> {
       ),
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
-          padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
-          child: Center(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisSize: MainAxisSize.min,
+   child: Column(
+     crossAxisAlignment: CrossAxisAlignment.stretch,
+     mainAxisSize: MainAxisSize.min,
+    children: <Widget> [
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 20.0),
+            height: 250,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
               children: <Widget>[
-                const ListTile(
-                  leading: Icon(Icons.book),
-                  title: Text(
-                      'O segundo livro de "Quem é você, Alasca? é anunciado'),
-                  subtitle: Text(
-                      'John Green está vivo! O escritor anunciou nesta Segunda-feira que seu livro mais popular'
-                      ' terá uma sequência.'),
-                ),
-                ButtonTheme.bar(
-                  // make buttons use the appropriate styles for cards
-                  child: ButtonBar(
-                    children: <Widget>[
-                      FlatButton(
-                        child: const Text('VER MAIS'),
-                        onPressed: () {},
-                      ),
-                      FlatButton(
-                        child: const Text('SALVAR'),
-                        onPressed: () {},
-                      ),
-                    ],
+                myNews("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQJWz7PeZmcNQp_svDznslTjfal2Oksb2j4VFwv3lzMw8WBWt3E", "Teste1", "Teste2"),
+                myNews("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTluY_u--YSm9Yi5k10bdzdoL0nKKcpC8Nxvs7T9rH2mfz8b_-v", "Teste1", "Teste2"),
+                myNews("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ1bKS_TTRyr8UryRNVN4OBQRdYkIQSwoPlDsPy8RysIRFX5w-B", "Teste1", "Teste2"),
+                myNews("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTWXpbiJWEeEGPuiqRuviFxjFJPrDB8UkmXsKHDsg_ATnh1rC4o", "Teste1", "Teste2"),
+                myNews("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRQkTQNBFPFC1e7qcXBgXXT-n4ToahKMj4I20I44AfbBAYgktzA", "Teste1", "Teste2"),
+      ],
+                    ),
                   ),
-                ),
-                Text("Tópicos"),
-                Divider(),
-        new GestureDetector(
-          onTap: (){
-            print("Container clicked");
-          },
-                child: Container(
-                    color: Color(0xff2A236E),
-                    padding: EdgeInsets.all(20.0),
-                    child: Text(
-                      "O que está acontecendo com J.K Rowling?",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                ),
-        ),
-        new GestureDetector(
-          onTap: (){
-            print("Container clicked");
-          },
-          child: Container(
-                    color: Colors.blue,
-                    padding: EdgeInsets.all(20.0),
-                    child: Text(
-                      "Como será a continuação de Mar de Monstros?",
-                      style: TextStyle(color: Colors.white),
-                    ),
+
+                              Text("Tópicos", textAlign: TextAlign.center,),
+                              Divider(),
+                              myTopic(Colors.blue, "Qual o segredo da bíblia?"),
+      myTopic(Color(0xff2A236E), "Quando sai a sequência de Game of Thrones?"),
+      myTopic(Colors.blue, "Netflix comprará Percy Jackson?"),
+      myTopic(Color(0xff2A236E), "SPOILER ALERT: Esse personagem vive ou morre?"),
+      FlatButton(
+        color: Color(0xff2A236E),
+        textColor: Colors.white,
+        disabledColor: Colors.grey,
+        disabledTextColor: Colors.black,
+        padding: EdgeInsets.all(8.0),
+        splashColor: Colors.blueAccent,
+        onPressed: () {
+          /*...*/
+        },
+        child: Text("Veja mais"),
+      ),
+      Divider(),
+      Text("Loja", textAlign: TextAlign.center,),
+      ButtonBar(
+          mainAxisSize: MainAxisSize.max,
+          alignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+          OutlineButton(
+          onPressed: () {},
+      child: Text("Novo"),
+      borderSide: BorderSide(color: Colors.blue),
+      shape: StadiumBorder(),
+    ),
+    OutlineButton(
+    onPressed: () {},
+    child: Text("Recomendado"),
+    borderSide: BorderSide(color: Colors.blue),
+    shape: StadiumBorder(),
+    ),
+    OutlineButton(
+    onPressed: () {},
+    child: Text("Menor Preço"),
+    borderSide: BorderSide(color: Colors.blue),
+    shape: StadiumBorder(),
+    ),
+                              ],
+      ),
+      Row(
+        children: <Widget>[
+          Expanded(
+      child: TextField(
+        textAlign: TextAlign.center,
+          decoration: InputDecoration(
+              border: InputBorder.none,
+              hintText: "Escreva o nome do livro"
           ),
-        ),
-        new GestureDetector(
-          onTap: (){
-            print("Container clicked");
-          },
-          child: Container(
-                    color: Color(0xff2A236E),
-                    padding: EdgeInsets.all(20.0),
-                    child: Text(
-                      "Seria a Bíblia uma mentira?",
-                      style: TextStyle(color: Colors.white),
-                    ),
-          ),
-        ),
-        new GestureDetector(
-          onTap: (){
-            print("LAIS OBRIGADO POR TUDO TE AMOOOO");
-          },
-          child: Container(
-                  color: Colors.blue,
-                  padding: EdgeInsets.all(20.0),
-                  child: Text(
-                    "Por que Game of Thrones é tão bom no livro?",
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-        ),
-                Divider(),
-                Text("Loja"),
-                ButtonBar(
-                  mainAxisSize: MainAxisSize.max,
-                  alignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    OutlineButton(
-                      onPressed: () {},
-                      child: Text("Novo"),
-                      borderSide: BorderSide(color: Colors.blue),
-                      shape: StadiumBorder(),
-                    ),
-                    OutlineButton(
-                      onPressed: () {},
-                      child: Text("Recomendado"),
-                      borderSide: BorderSide(color: Colors.blue),
-                      shape: StadiumBorder(),
-                    ),
-                    OutlineButton(
-                      onPressed: () {},
-                      child: Text("Menor Preço"),
-                      borderSide: BorderSide(color: Colors.blue),
-                      shape: StadiumBorder(),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          )),
+      ),
+    ),
+          IconButton(
+            icon: Icon(Icons.send),
+            color: Colors.blue,
+            onPressed: () {},
+          )
+    ],
+   ),
+      ],
+    ),
+      ),
     );
   }
 }
